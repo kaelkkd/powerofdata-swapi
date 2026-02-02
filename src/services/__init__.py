95db3595c47e9d5d4177c00f71a7841a1c8c7408
kael <kaelsiebra233@gmail.com>
2026-02-02 13:55:27 -0300

    fix: module exporting

---
+++ src/services/__init__.py
@@ -0,0 +1,15 @@
+from .people_service import PeopleService
+from .film_service import FilmService
+from .planet_service import PlanetService
+from .specie_service import SpecieService
+from .starship_service import StarshipService
+from .vehicle_service import VehicleService
+
+__all__ = [
+    "PeopleService",
+    "FilmService",
+    "PlanetService",
+    "SpecieService",
+    "StarshipService",
+    "VehicleService",
+]
